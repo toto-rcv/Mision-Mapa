e0cddb22ca93998f37f825e497aeff1938fac4ee
Cristo <blackflame@gmail.com>
2024-12-16 20:59:10 -0300

    Añadimos el UserRanks y actualizacion de refreshTokens y accesTokens

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,63 +1,64 @@
 const { hashPassword, comparePassword } = require("../utils/password.util");
-const { generateAccessToken,generateRefreshToken } = require("../utils/jwt.util");
+const { generateAccessToken, generateRefreshToken } = require("../utils/jwt.util");
 const { saveRefreshToken } = require("../utils/refreshTokens");
 const db = require("../models");
 const User = db.User;
 
 exports.register = async (req, res) => {
-    try {
-        const { dni, email, password, firstName, lastName, militaryRank } = req.body;
+  try {
+    const { dni, email, password, firstName, lastName, militaryRank } = req.body;
 
-        // Validación de correo y DNI únicos
-        const existingUser = await User.findOne({ where: { email } });
-        const existingDNI = await User.findOne({ where: { dni } });
+    console.log(req.body)
+    // Validación de correo y DNI únicos
+    const existingUser = await User.findOne({ where: { email } });
+    const existingDNI = await User.findOne({ where: { dni } });
 
-        if (existingUser) {
-            return res.status(400).json({ message: "El correo electrónico ya está en uso" });
-        }
-
-        if (existingDNI) {
-            return res.status(400).json({ message: "El DNI ya está registrado" });
-        }
-
-        // Hashear la contraseña
-        const hashedPassword = await hashPassword(password);
-
-        // Crear el usuario
-        const newUser = await User.create({
-            dni,
-            email,
-            password: hashedPassword,
-            firstName,
-            lastName,
-            militaryRank
-        });
-
-        res.status(201).json({ message: "Usuario registrado exitosamente", user: newUser });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (existingUser) {
+      return res.status(400).json({ message: "El correo electrónico ya está en uso" });
     }
+
+    if (existingDNI) {
+      return res.status(400).json({ message: "El DNI ya está registrado" });
+    }
+
+    // Hashear la contraseña
+    const hashedPassword = await hashPassword(password);
+
+    // Crear el usuario
+    const newUser = await User.create({
+      dni,
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      militaryRank,
+    });
+
+    res.status(201).json({ message: "Usuario registrado exitosamente", user: newUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
-    if (!user) return res.status(401).json({ message: "Credenciales Incorrectas" });
+  const user = await User.findOne({ where: { email } });
+  if (!user) return res.status(401).json({ message: "Credenciales Incorrectas" });
 
-    const isValidPassword = await comparePassword(password, user.password);
-    if (!isValidPassword) return res.status(401).json({ message: "Credenciales Incorrectas" });
+  const isValidPassword = await comparePassword(password, user.password);
+  if (!isValidPassword) return res.status(401).json({ message: "Credenciales Incorrectas" });
 
-    const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
 
-    saveRefreshToken(refreshToken)
+  saveRefreshToken(refreshToken)
 
-    res.json({
-        message: "Acceso exitoso",
-        accessToken,
-        refreshToken,
-    });
+  res.json({
+    message: "Acceso exitoso",
+    accessToken,
+    refreshToken,
+  });
 
 };
 
@@ -77,13 +78,20 @@ exports.verifyToken = (req, res) => {
 };
 
 // Generar nuevo access token usando refresh token
-exports.refreshAccessToken = (req, res) => {
+exports.refreshAccessToken = async (req, res) => {
   const user = req.user; // Datos decodificados del Refresh Token
+  
+  const dni = user.id
+  const userR = await User.findOne({ where: { dni } });
+  if (!userR) return res.status(401).json({ message: "Flaco es aca" });
+
+  console.log(req.user)
+
 
   // Generar un nuevo Access Token
-  const newAccessToken = generateAccessToken({ id: user.id, email: user.email });
+  const newAccessToken = generateAccessToken({ dni: userR.dni, email: userR.email, userRank: userR.userRank });
 
   return res.status(200).json({
-      accessToken: newAccessToken,
+    accessToken: newAccessToken,
   });
 };
--- Backend/src/utils/jwt.util.js
+++ Backend/src/utils/jwt.util.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken");
 //funcion para generar los tokens
 exports.generateAccessToken = (user) => {
     return jwt.sign(
-      { id: user.dni, email: user.email },
+      { id: user.dni, email: user.email, rank: user.userRank},
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "10m" }
+      { expiresIn: "15m" }
     );
   };
   
