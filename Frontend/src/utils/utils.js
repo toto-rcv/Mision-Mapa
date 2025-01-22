6998c3019e681280b18271de3af59782f40d58ad
Nachlord <ignaciomartinez.dev@gmail.com>
2025-01-21 21:26:22 -0300

    Refactor de records, taba feo :(

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -5,3 +5,28 @@ export function toProperCase(text) {
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(" "); 
 }
+
+export function debounce(fn, delay = 1000) {
+    let timerId = null;
+    return (...args) => {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => fn(...args), delay);
+    };
+};
+
+
+export function formatDate(date) {
+    const day = date.getDate(); // Día (1-31)
+    const monthIndex = date.getMonth(); // Mes (0-11)
+    const year = date.getFullYear(); // Año (4 dígitos)
+    const months = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+
+    const formattedDate = `${day}/${months[monthIndex]}/${year}`
+    const formattedTime = new Intl.DateTimeFormat('es-ES', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    }).format(date);
+
+    return `${formattedDate} ${formattedTime}`;
+}
