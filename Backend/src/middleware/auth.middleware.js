afa543ff791288e1c412ddaac47a0468ed643316
Nachlord <ignaciomartinez.dev@gmail.com>
2025-02-13 12:31:28 -0300

    Validación de campos

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,8 +1,64 @@
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
 
 const { existsRefreshToken } = require("../utils/refreshTokens");
 
 require("dotenv").config();
+
+exports.validateLogin = [
+  body('email')
+  .exists().withMessage('El campo email es obligatorio')
+  .isString()
+  .bail()
+  .isEmail().withMessage('El email no es válido'),
+
+  body('password')
+    .exists().withMessage('El campo password es obligatorio')
+    .isString(),
+    
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+  }
+];
+
+exports.validateRegister = [
+  body('dni')
+    .exists().withMessage('El campo dni es obligatorio'),
+
+  body('email')
+    .exists().withMessage('El campo email es obligatorio')
+    .isString().withMessage('El campo email debe ser un string')
+    .bail() // Detiene la cadena de validaciones si falla lo anterior
+    .isEmail().withMessage('El email no es válido'),
+  
+  body('password')
+    .exists().withMessage('El campo password es obligatorio')
+    .isString().withMessage('El campo password debe ser un string'),
+
+  body('firstName')
+    .exists().withMessage('El campo firstName es obligatorio')
+    .isString().withMessage('El campo firstName debe ser un string'),
+
+  body('lastName')
+    .exists().withMessage('El campo lastName es obligatorio')
+    .isString().withMessage('El campo lastName debe ser un string'),
+
+  body('militaryRank')
+    .exists().withMessage('El campo militaryRank es obligatorio')
+    .isString().withMessage('El campo militaryRank debe ser un string'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
 
 exports.validateAccessToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login, refreshAccessToken, getProfile } = require("../controllers/auth.controller");
-const {validateRefreshToken, validateAccessToken} = require("../middleware/auth.middleware");
+const { validateLogin, validateRegister, validateRefreshToken, validateAccessToken } = require("../middleware/auth.middleware");
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateRegister, register);
+router.post("/login", validateLogin, login);
 router.post("/refresh-token", validateRefreshToken, refreshAccessToken);
 
 router.get("/profile", validateAccessToken, getProfile);
