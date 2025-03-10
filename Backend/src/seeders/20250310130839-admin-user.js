d1cfb8d7bf6ea9b657f1b7f2ab92667b2b9f2bb0
Nachlord <ignaciomartinez.dev@gmail.com>
2025-03-10 10:18:07 -0300

    Seed de usuario administrador

---
+++ Backend/src/seeders/20250310130839-admin-user.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const adminDNI = 1000000;
+    const adminEmail = process.env.ADMIN_EMAIL || 'admin@example.com';
+    const adminPassword = process.env.ADMIN_PASSWORD || 'adminpassword123';
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
+
+    // Verificar si ya existe el usuario administrador
+    const [admin] = await queryInterface.sequelize.query(
+      `SELECT dni FROM Users WHERE email = '${adminEmail}' LIMIT 1;`
+    );
+
+    if (admin.length === 0) {
+      return queryInterface.bulkInsert('Users', [
+        {
+          dni: adminDNI,
+          email: adminEmail,
+          password: hashedPassword,
+          firstName: 'Admin',
+          lastName: 'User',
+          militaryRank: 'Administrator',
+          userRank: 'JEFE DE DETECCION',
+          status: 2
+        }
+      ]);
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', { dni: 1000000 });
+  }
+};
